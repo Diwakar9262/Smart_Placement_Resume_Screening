a3571e8799c4e220f17e9ad714e1413c1fbfc574
Diwakar9262 <diwakarkumarupa9@gmail.com>
2026-07-14 22:37:44 +0530

    Day 14: Added CSV export and Pandas integration

--- test_pandas.py
+++ test_pandas.py
@@ -3,3 +3,15 @@ import pandas as pd
 df = pd.read_csv("data/candidates.csv")
 
 print(df)
+
+print("\nRows and Columns")
+print(df.shape)
+
+print("\nColumn Names")
+print(df.columns)
+
+print("\nFirst 5 Rows")
+print(df.head())
+
+print("\nInformation")
+print(df.info())
