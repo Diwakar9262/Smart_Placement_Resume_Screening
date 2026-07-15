7d7cc0a72c9ded6b155d51b9111ddba281876656
Diwakar9262 <diwakarkumarupa9@gmail.com>
2026-07-15 16:20:22 +0530

    Day 31: Started Streamlit Web Application

---
+++ app/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Smart Placement ATS",
+    page_icon="📄",
+    layout="wide"
+)
+
+st.title("📄 Smart Placement & Resume Screening Engine")
+
+st.subheader("AI Powered Applicant Tracking System")
+
+st.write("Welcome to your ATS Project!")
+
+st.success("Day 31 Started Successfully 🚀")
