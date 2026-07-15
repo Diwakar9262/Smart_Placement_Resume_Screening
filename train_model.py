be3f32b2ba37a760477fa0d1a00704f562cad35f
Diwakar9262 <diwakarkumarupa9@gmail.com>
2026-07-15 12:10:08 +0530

    Day 17: Added train-test split and model evaluation

--- train_model.py
+++ train_model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 df = pd.read_csv("data/resume_dataset.csv")
 
@@ -7,9 +9,24 @@ X = df[["Skills", "Age"]]
 
 y = df["ResumeScore"]
 
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 model = LinearRegression()
 
-model.fit(X, y)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Predictions:")
+print(y_pred)
+
+mae = mean_absolute_error(y_test, y_pred)
+
+print("Mean Absolute Error =", mae)
 
 new_candidate = pd.DataFrame({
     "Skills": [5],
